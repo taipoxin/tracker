a25c8460b53857056db7c76dcfdfbd58746d31fc
tiranid99 <ermakovichdmitriy@gmail.com>
2017-10-01 10:02:27 +0300

    fixed db_url

--- tiranid-container/src/main/java/by/tiranid/api/controller/MainRestController.java
+++ tiranid-container/src/main/java/by/tiranid/api/controller/MainRestController.java
@@ -11,37 +11,28 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.Date;
 import java.sql.Time;
 
 
-
 @RestController
 public class MainRestController {
 
+    private static final Logger log = LoggerFactory.getLogger(HttpLoggingFilter.class);
+    private static int userHash = -442696469;
     @Autowired
     private WorkDaysRepository workDaysRepository;
-
     private WorkDaysService workDaysService;
-
     @Autowired
     private WorkItersRepository workItersRepository;
-
     private WorkItersService workItersService;
-
-
-    private static int userHash = -442696469;
-
-    private static final Logger log = LoggerFactory.getLogger(HttpLoggingFilter.class);
-
-
-
 
     @RequestMapping(value = {"/postIter"}, method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.ACCEPTED)
-    public void postIter(@RequestParam String hash, @RequestParam String time){
+    public ResponseEntity.BodyBuilder postIter(@RequestParam String hash, @RequestParam String time) {
         log.info("retrieved hash: " + hash);
         log.info("retrieved time: " + time);
         if (Integer.valueOf(hash) == -442696469) {
@@ -53,21 +44,23 @@ public class MainRestController {
             WorkItersEntity entity = new WorkItersEntity();
             entity.setDdate(new Date(t));
             entity.setTtime(new Time(t));
-            log.info("adding new entity: " );
+            log.info("adding new entity: ");
             log.info(entity.getDdate().toString());
             log.info(entity.getTtime().toString());
             workItersService.addRecord(entity);
-        }
-        else {
+            return ResponseEntity.ok();
+        } else {
             log.warn("sorry");
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED);
         }
     }
 
-    @RequestMapping(value = {"/"}, method = RequestMethod.GET)
+    @RequestMapping(value = {"/postIter"}, method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.ACCEPTED)
-    public void gett(){
-        log.info("get get get");
-        postIter(Long.toString(userHash), Long.toString(new java.util.Date().getTime()));
+    public ResponseEntity.BodyBuilder getToPostIter() {
+        log.info("get request to postIter");
+        log.info("Returning OK Http Code");
+        return ResponseEntity.ok();
     }
 
 
--- tiranid-dao/src/test/java/by/tiranid/config/TestConfigParams.java
+++ tiranid-dao/src/test/java/by/tiranid/config/TestConfigParams.java
@@ -6,7 +6,7 @@ public class TestConfigParams {
     // TestDataConfig
     public static final String username = "tiranid";
     public static final String password = "6559520";
-    public static final String db_url = "jdbc:mysql://localhost:3306/test";
+    public static final String db_url = "jdbc:mysql://localhost:3306/test?verifyServerCertificate=false&useSSL=true";
     public static final String db_driver = "com.mysql.jdbc.Driver";
 
     public static final String hibernate_dialect = "org.hibernate.dialect.MySQLDialect";
